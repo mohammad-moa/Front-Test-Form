b9563240390c8e1a46c2868ec8b3bb6a9a85d51e
mohammad-moa <aahrari5@gmail.com>
2023-06-13 09:40:12 +0330

    added top banner and logo image ui in top page

--- src/components/TopBanner/index.tsx
+++ src/components/TopBanner/index.tsx
@@ -1,5 +1,21 @@
+import Image from "../Common/Image";
+
+import topBanner from "../../assets/images/top-banner.svg";
+import logo from "../../assets/images/logo.svg";
+import { Box } from "@mui/material";
+
 const TopBanner: React.FC = () => {
-  return <div>TopBanner</div>;
+  return (
+    <Box position={"relative"}>
+      <Image
+        src={logo}
+        alt={"logo-image"}
+        width={"10%"}
+        style={{ position: "absolute", top: "15px", left: "15px" }}
+      />
+      <Image src={topBanner} alt={"top-banner-image"} width={"100%"} />
+    </Box>
+  );
 };
 
 export default TopBanner;
